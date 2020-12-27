11708675e5621b95b371b633fe693c7ad477f428
TEJAS <gaikar.tejas22@gmgail.com>
2020-12-27 11:02:31 +0530

    First Commit

---
+++ src/main/java/com/story/UserStorytask/CDVRouteEvent2.java
@@ -0,0 +1,14 @@
+package com.story.UserStorytask;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CDVRouteEvent2 {
+	private String ID;
+	private String name;
+
+}
